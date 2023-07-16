33afc5f2cdbd1ce01e8cc95cc337880a0d3b3995
Satoshi Matos <maylsonmatos@gmail.com>
2023-07-15 21:01:15 -0300

    maps entities according to their schema

---
+++ src/entity/items.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm"
+import { Locations } from "./locations"
+
+@Entity('item')
+
+export class Items {
+    @PrimaryGeneratedColumn()
+    item_id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    rating: number
+
+    @Column()
+    category: number
+
+    @Column()
+    image: string
+
+    @Column()
+    reputation: number
+
+    @Column()
+    reputation_badge: string
+
+    @Column()
+    price: number
+
+    @Column()
+    availability: number
+
+    @ManyToOne(() => Locations)
+    @JoinColumn({ name: 'location_id' })
+    location: Locations;
+}
---
+++ src/entity/locations.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('locations')
+
+export class Locations {
+    @PrimaryGeneratedColumn()
+    location_id: number;
+
+    @Column()
+    city: string;
+
+    @Column()
+    state: string;
+
+    @Column()
+    country: string;
+
+    @Column()
+    zip_code: string;
+
+    @Column()
+    address: string;
+}
