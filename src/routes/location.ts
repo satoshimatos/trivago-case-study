b3d549f7627f88c68d3788ab9aa5cbe738be859f
Satoshi Matos <maylsonmatos@gmail.com>
2023-07-18 15:07:33 -0300

    adds endpoint to retrieve all locations

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -3,6 +3,21 @@ import { Router, Request, Response } from 'express'
 const locationController = require('../controllers/LocationController')
 const router = Router()
 
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        let result = await locationController.getList()
+        res.status(200).json({
+            "success": true,
+            "items": result
+        })
+    } catch (error) {
+        res.status(400).json({
+            "success": false,
+            "message": error.message
+        })
+    }
+})
+
 router.get('/:id', async (req: Request, res: Response) => {
     try {
         let result = await locationController.getOneById(req.params.id)
